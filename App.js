4c2edfdd4e8b0562799f60e280969235a2f36107
sabahat-123 <sabahat.parveen@imtspl.net>
2022-11-27 22:22:53 +0530

    initial commit

--- App.js
+++ App.js
@@ -7,10 +7,10 @@ codes.forEach((code, index) => {
     code.addEventListener('keydown' , (e) => {
         if(e.key >= 0 && e.key <= 9){
             codes[index].value = '';
-           setTimeout(() => codes[index + 1].focus(),10);
+            setTimeout(() => codes[index + 1].focus(), 10);
 
         }else if(e.key === 'Backspace'){
-            setTimeout(() => codes[index - 1].focus(),10);
+            setTimeout(() => codes[index - 1].focus(), 10);
 
         }
 
