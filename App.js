5003c9cdd66e969b98e7c8a34cd32049c3b4c247
sabahat-123 <sabahat.parveen@imtspl.net>
2022-11-30 00:43:15 +0530

    modified

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-const codes = document.querySelectorAll('code');
+const codes = document.querySelectorAll('.code');
 
 codes[0].focus();
 
-codes.forEach((code, index) => {
+codes.forEach((code, idx) => {
 
-    code.addEventListener('keydown' , (e) => {
+    code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <= 9){
-            codes[index].value = '';
-            setTimeout(() => codes[index + 1].focus(), 10);
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10);
 
         }else if(e.key === 'Backspace'){
-            setTimeout(() => codes[index - 1].focus(), 10);
+            setTimeout(() => codes[idx - 1].focus(), 10);
 
         }
 
